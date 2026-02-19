a144838c7e230933540c5f64e2801c3b8a1f10b5
Ilya Mikhaltsou <morpheby@gmail.com>
2026-02-20 00:36:26 +0300

    Add a small stub so that clangd doesn't complain

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -25,7 +25,7 @@
                      + __GNUC_MINOR__ * 100 \
                      + __GNUC_PATCHLEVEL__)
 #endif
-#if GCC_VERSION < 60300
+#if !defined(__clang__) && GCC_VERSION < 60300
   #error "GCC version 6.3 or higher is required"
 #endif
 
