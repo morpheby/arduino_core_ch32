c6eee667bb6b5905a289df1d64c5a09b6cbda247
Ilya Mikhaltsou <morpheby@gmail.com>
2026-02-23 02:01:10 +0300

    Move default _write implementation to HAL as it depends on newlib/picolibc

--- cores/arduino/Print.cpp
+++ cores/arduino/Print.cpp
@@ -232,62 +232,6 @@ size_t Print::println(const Printable &x)
   return n;
 }
 
-extern "C" {
-   __attribute__((weak))
-  int _write(int file, char *ptr, int len)
-  {
-    switch (file) {
-      case STDOUT_FILENO:
-      case STDERR_FILENO:
-      case STDIN_FILENO: // This doesn't make sense, but something somewhere is broken in printf call
-        #if defined(UART_MODULE_ENABLED) && !defined(UART_MODULE_ONLY)
-        uart_debug_write((uint8_t *)ptr, (uint32_t)len);
-        #elif (SDI_PRINT == SDI_PR_OPEN)
-        {
-          int writeSize = len;
-          int i = 0;
-      
-          do
-          {
-      
-              /**
-               * data0  data1 8 byte
-               * data0 The storage length of the lowest byte, with a maximum of 7 bytes.
-               */
-      
-              while( (*(DEBUG_DATA0_ADDRESS) != 0u))
-              {
-      
-              }
-      
-              if(writeSize>7)
-              {
-                  *(DEBUG_DATA1_ADDRESS) = (*(ptr+i+3)) | (*(ptr+i+4)<<8) | (*(ptr+i+5)<<16) | (*(ptr+i+6)<<24);
-                  *(DEBUG_DATA0_ADDRESS) = (7u) | (*(ptr+i)<<8) | (*(ptr+i+1)<<16) | (*(ptr+i+2)<<24);
-      
-                  i += 7;
-                  writeSize -= 7;
-              }
-              else
-              {
-                  *(DEBUG_DATA1_ADDRESS) = (*(ptr+i+3)) | (*(ptr+i+4)<<8) | (*(ptr+i+5)<<16) | (*(ptr+i+6)<<24);
-                  *(DEBUG_DATA0_ADDRESS) = (writeSize) | (*(ptr+i)<<8) | (*(ptr+i+1)<<16) | (*(ptr+i+2)<<24);
-      
-                  writeSize = 0;
-              }
-      
-          } while (writeSize);        
-        }
-        #endif
-        break;
-      default:
-        ((class Print *)file)->write((uint8_t *)ptr, len);
-        break;
-    }
-    return len;
-  }
-}
-
 int Print::printf(const char *format, ...)
 {
   va_list ap;
