d863cefd0af457e7564647883907bd89630bddf1
Ilya Mikhaltsou <morpheby@gmail.com>
2026-04-07 14:49:23 +0300

    WIP: Disable UART init for now

--- cores/arduino/HardwareSerial.cpp
+++ cores/arduino/HardwareSerial.cpp
@@ -137,7 +137,7 @@ void HardwareSerial::begin(unsigned long baud, byte config)
       Error_Handler();
       break;
   }
-  uart_init(&_serial, (uint32_t)baud, databits, parity, stopbits);
+  // uart_init(&_serial, (uint32_t)baud, databits, parity, stopbits);
 }
 
 void HardwareSerial::end()
