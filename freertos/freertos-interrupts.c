867032d0bf6014b99fce6fbd3c63f0192b523bd2
Ilya Mikhaltsou <morpheby@gmail.com>
2026-06-05 03:41:25 +0300

    Update FreeRTOS ISR handlers

--- freertos/freertos-interrupts.c
+++ freertos/freertos-interrupts.c
@@ -5,24 +5,38 @@
 #include <stdint.h>
 #include <assert.h>
 
+#ifndef NDEBUG 
+    #define DBGREG volatile
+#endif
+
 #if CH32_GLOBAL_ISR
 _ISR_DEF(freertos_risc_v_application_exception_handler) {
-    uint32_t mcause, mepc, mstatus;
+    DBGREG uint32_t mcause;
+    
     __asm__ volatile ("csrr %0, mcause" : "=r"(mcause));
+    
+    #ifndef NDEBUG
+    DBGREG uint32_t mepc, mstatus;
     __asm__ volatile ("csrr %0, mepc" : "=r"(mepc));
     __asm__ volatile ("csrr %0, mstatus" : "=r"(mstatus));
+    #endif
     
-    assert((mcause & 0x7FFFFFFF) < CH32_EXCEPTION_TABLE_SIZE);
-    __MCU_Exceptions[mcause & 0x7FFFFFFF]();
+    assert((mcause & 0x7FFFFFFFu) < CH32_EXCEPTION_TABLE_SIZE);
+    __MCU_Exceptions[mcause & 0x7FFFFFFFu]();
 }
 
 _ISR_DEF(freertos_risc_v_application_interrupt_handler) {
-    uint32_t mcause, mepc, mstatus;
+    DBGREG uint32_t mcause;
+    
     __asm__ volatile ("csrr %0, mcause" : "=r"(mcause));
+    
+    #ifndef NDEBUG
+    DBGREG uint32_t mepc, mstatus;
     __asm__ volatile ("csrr %0, mepc" : "=r"(mepc));
     __asm__ volatile ("csrr %0, mstatus" : "=r"(mstatus));
+    #endif
     
-    assert((mcause & 0x7FFFFFFF) < CH32_IRQ_TABLE_SIZE);
-    __MCU_Interrupts[mcause & 0x7FFFFFFF]();
+    assert((mcause & 0x7FFFFFFFu) < CH32_IRQ_TABLE_SIZE);
+    __MCU_Interrupts[mcause & 0x7FFFFFFFu]();
 }
 #endif
